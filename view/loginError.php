79c948c1208c7c509ccee23b2b59774c31550c6d
mvillalva <maxi.villalva@gmail.com>
2022-07-08 16:43:34 -0300

    Modificación de Titles

--- view/loginError.php
+++ view/loginError.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>PHP Login</title>
+    <title>PHP - Login</title>
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
--- view/pedido.php
+++ view/pedido.php
@@ -8,7 +8,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <title>Registro de Pedido</title>
+    <title>PHP - Registro de Pedido</title>
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" />
