b8535558cc3c2608181a28c4fe315a037f333280
mvillalva <maxi.villalva@gmail.com>
2022-07-12 19:57:32 -0300

    Config

--- dataBase/ConfigDB.php
+++ dataBase/ConfigDB.php
@@ -1,6 +1,6 @@
 <?php 
-  const DB_HOST = '192.168.0.101';
+  const DB_HOST = 'localhost';
   const DB_PORT = 3306;
   const DB_DATABASENAME = 'cacproyecto';
-  const DB_USER = 'remoto';
-  const DB_PASS = 'Remoto123!';
+  const DB_USER = 'root';
+  const DB_PASS = '';
